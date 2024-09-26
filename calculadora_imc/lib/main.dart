2278a6969bc8e93dcfb3248df69f87fecbfd9113
matheusedivado <ms1501085@gmail.com>
2024-09-26 17:57:32 -0300

    Imagens Locais

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -27,6 +27,7 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
   final _controladorPeso = TextEditingController();
   String _resultado = "";
   String _classificacao = "";
+  String _imagemURL = "assets/images/default_image.png"; // Imagem padrão inicial
   bool _campoAtivoPeso = false;
 
   @override
@@ -53,6 +54,7 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
       setState(() {
         _resultado = "IMC: ${imc.toStringAsFixed(2)}";
         _classificacao = _classificarIMC(imc);
+        _imagemURL = _obterImagemPorIMC(imc); // Atualiza a imagem com base no IMC
       });
     } else {
       _mostrarMensagemErro("Por favor, insira valores válidos.");
@@ -79,8 +81,30 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
     }
   }
 
+  // Função para obter a URL da imagem com base no IMC
+  String _obterImagemPorIMC(double imc) {
+    if (imc < 16) {
+      return 'assets/images/obesidade_grau_3.png'; // Magreza Grau III
+    } else if (imc >= 16 && imc <= 16.9) {
+      return 'assets/images/obesidade_grau_2.png'; // Magreza Grau II
+    } else if (imc >= 17 && imc <= 18.4) {
+      return 'assets/images/magreza_grau_1.png'; // Magreza Grau I
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return 'assets/images/eutrofia.png'; // Eutrofia
+    } else if (imc >= 25 && imc <= 29.9) {
+      return 'assets/images/pre_obesidade.png'; // Pré-obesidade
+    } else if (imc >= 30 && imc <= 34.9) {
+      return 'assets/images/obesidade_grau_1.png'; // Obesidade Moderada
+    } else if (imc >= 35 && imc <= 39.9) {
+      return 'assets/images/obesidade_grau_2.png'; // Obesidade Severa
+    } else {
+      return 'assets/images/obesidade_grau_3.png'; // Obesidade Muito Severa
+    }
+  }
+
   void _mostrarMensagemErro(String mensagem) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mensagem)));
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(mensagem)));
   }
 
   void _atualizarCampo(String valor) {
@@ -97,11 +121,13 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
     setState(() {
       if (_campoAtivoPeso) {
         _controladorPeso.text = _controladorPeso.text.isNotEmpty
-            ? _controladorPeso.text.substring(0, _controladorPeso.text.length - 1)
+            ? _controladorPeso.text
+                .substring(0, _controladorPeso.text.length - 1)
             : '';
       } else {
         _controladorAltura.text = _controladorAltura.text.isNotEmpty
-            ? _controladorAltura.text.substring(0, _controladorAltura.text.length - 1)
+            ? _controladorAltura.text
+                .substring(0, _controladorAltura.text.length - 1)
             : '';
       }
     });
@@ -118,14 +144,14 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calculadora de IMC', textAlign: TextAlign.center),
-        centerTitle: true, // Alinhar o título no centro
+        centerTitle: true,
       ),
-      body: SingleChildScrollView( // Adicionado para permitir rolagem
+      body: SingleChildScrollView(
         child: Center(
           child: Container(
-            width: 350, // Largura da calculadora
-            margin: const EdgeInsets.all(10), // Margem menor
-            padding: const EdgeInsets.all(20), // Espaçamento interno maior
+            width: 350,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: Colors.white,
@@ -134,7 +160,7 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
                   color: Colors.grey.withOpacity(0.2),
                   spreadRadius: 2,
                   blurRadius: 5,
-                  offset: Offset(0, 3), // Sombra abaixo
+                  offset: Offset(0, 3),
                 ),
               ],
             ),
@@ -145,7 +171,7 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
                 _construirImagem(),
                 const SizedBox(height: 20),
                 _construirCampoTexto(_controladorAltura, 'Altura (cm)', false),
-                const SizedBox(height: 10), // Espaçamento entre os campos
+                const SizedBox(height: 10),
                 _construirCampoTexto(_controladorPeso, 'Peso (kg)', true),
                 const SizedBox(height: 20),
                 ElevatedButton(
@@ -166,6 +192,12 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
                 ),
                 const SizedBox(height: 20),
                 _construirTecladoNumerico(),
+                const SizedBox(height: 20),
+                const Text(
+                  'Desenvolvido por Matheus Edivaldo',
+                  style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+                  textAlign: TextAlign.center,
+                ),
               ],
             ),
           ),
@@ -177,21 +209,22 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
   Widget _construirImagem() {
     return Center(
       child: Container(
-        height: 200, // Altura da imagem
-        width: 200,  // Largura da imagem
+        height: 200, // Altura da imagem padrão
+        width: 100,  // Largura da imagem padrão
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: NetworkImage(
-              'https://play-lh.googleusercontent.com/ouL1lfSP_CyUgb5OUvI51jG3cevMfulA1GZGtS63r3Xfa8STYiIxq6KiY3PkMc6PcTk=w240-h480-rw',
-            ),
-            fit: BoxFit.cover,
+            image: NetworkImage(_imagemURL.isEmpty
+                ? 'https://play-lh.googleusercontent.com/ouL1lfSP_CyUgb5OUvI51jG3cevMfulA1GZGtS63r3Xfa8STYiIxq6KiY3PkMc6PcTk=w240-h480-rw'
+                : _imagemURL),
+            fit: BoxFit.fill, // Preencher o espaço da imagem
           ),
         ),
       ),
     );
   }
 
-  Widget _construirCampoTexto(TextEditingController controlador, String rotulo, bool ativoPeso) {
+  Widget _construirCampoTexto(
+      TextEditingController controlador, String rotulo, bool ativoPeso) {
     return TextField(
       controller: controlador,
       decoration: InputDecoration(
@@ -207,7 +240,7 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
 
   Widget _construirTecladoNumerico() {
     List<List<String>> teclas = [
-      ['7', '8', '9', 'DEL'],
+      ['7', '8', '9', '<'],
       ['4', '5', '6', '.'],
       ['1', '2', '3', '0'],
     ];
@@ -217,34 +250,26 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
         return Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: linha.map((tecla) {
-            return _construirBotaoTeclado(tecla);
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: SizedBox(
+                width: 50, // Largura do botão
+                height: 50, // Altura do botão
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (tecla == '<') {
+                      _deletarUltimoCaracter();
+                    } else {
+                      _atualizarCampo(tecla);
+                    }
+                  },
+                  child: Text(tecla, style: const TextStyle(fontSize: 20)),
+                ),
+              ),
+            );
           }).toList(),
         );
       }).toList(),
-    );
-  }
-
-  Widget _construirBotaoTeclado(String texto) {
-    return ElevatedButton(
-      onPressed: () {
-        if (texto == 'DEL') {
-          _deletarUltimoCaracter();
-        } else {
-          _atualizarCampo(texto);
-        }
-      },
-      style: ElevatedButton.styleFrom(
-        fixedSize: const Size(60, 60),
-      ),
-      child: texto == 'DEL'
-          ? const Icon(Icons.backspace, size: 24)
-          : Text(
-              texto,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-              ),
-            ),
     );
   }
 }
